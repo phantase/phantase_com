85950dd14dff0ee514806636add15987f62f472d
Phantase <phantase@free.fr>
2018-01-30 22:25:05 +0100

    piwik code

--- html/views/skeleton_bottom.php
+++ html/views/skeleton_bottom.php
@@ -66,20 +66,20 @@
 				&copy; Phantase. Tous droits réservés.
 			</div>
 
-			<!-- Piwik -->
+<!-- Piwik -->
 <script type="text/javascript">
   var _paq = _paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
-    var u="//www.phantase.com/stats/";
+    var u="//stats.phantase.com/";
     _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 1]);
+    _paq.push(['setSiteId', '3']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>
-<noscript><p><img src="//www.phantase.com/stats/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
 <!-- End Piwik Code -->
 
 	</body>
